3ebe080a7b86e3a72da6805c7cdbf2c63580992e
JPriskomatis <JPriskomatis@gmail.com>
2025-01-25 16:08:18 +0200

    Fix: hedgehog now goes to the position he pointed to

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,6 +12,7 @@ public abstract class Enemy : MonoBehaviour
 
     protected bool seePlayer;
     protected Transform playerTransform;
+    protected bool tookPlayersPos;
 
     //This is the vision range of each enemy (the collider)
     private void OnTriggerEnter(Collider other)
--- Assets/Scripts/Enemy/EnemyHedgehog.cs
+++ Assets/Scripts/Enemy/EnemyHedgehog.cs
@@ -10,22 +10,24 @@ public class EnemyHedgehog : Enemy
 
     public override void AttackPlayer(Transform playerTransform)
     {
-        playerPos = playerTransform.position;
         if (seePlayer)
         {
             anim.SetTrigger("Point");
         }
     }
 
+    //This function is called from the Animator as an event;
     private void StartRolling()
     {
         //Move towards player;
+        tookPlayersPos = true;
         StartCoroutine(MoveToPlayer(playerPos, attackSpeed));
         anim.SetTrigger("Roll");
     }
 
     IEnumerator MoveToPlayer(Vector3 targetPos, float speed)
     {
+        targetPos = playerTransform.position;
         while (Vector3.Distance(transform.position, targetPos) > 0.01f)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
