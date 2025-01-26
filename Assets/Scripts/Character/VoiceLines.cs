48f43ebe5c09987e4f221fcec16be6f162eb0fcd
JPriskomatis <JPriskomatis@gmail.com>
2025-01-26 13:44:39 +0200

    Add: Voice lines every 30 seconds

--- Assets/Scripts/Character/VoiceLines.cs
+++ Assets/Scripts/Character/VoiceLines.cs
@@ -5,6 +5,9 @@ public class VoiceLines : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private float delay;
+
+    private AudioClip lastClip;
 
     private void Start()
     {
@@ -17,16 +20,24 @@ public class VoiceLines : MonoBehaviour
         {
             if (audioClips.Length > 0)
             {
-                
+                yield return new WaitForSeconds(delay);
+
                 AudioClip randomClip = audioClips[Random.Range(0, audioClips.Length)];
 
-                
+                while (randomClip == lastClip)
+                {
+                    randomClip = audioClips[Random.Range(0, audioClips.Length)];
+                }
+
+                lastClip = randomClip;
+
+
                 audioSource.clip = randomClip;
                 audioSource.Play();
             }
 
             
-            yield return new WaitForSeconds(10f);
+            
         }
     }
 
